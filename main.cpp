01611e9d0ba560a1648239a1f627de9a9b154f2a
ariel <71622992+ariiel1@users.noreply.github.com>
2021-06-28 00:13:20 +0700

    finalized code

--- main.cpp
+++ main.cpp
@@ -12,28 +12,40 @@ int main (){
         cin>>option;
         
         switch (option){
+        	//ADD CONTACT
 	        case 1:
 	            addContact();
 	            system("cls");
 	            break;
+	        //DISPLAY CONTACT
 	        case 2:
 	            display();
 	            cin.get(pause);
 	            system("cls");
 	            len = 0;
 	            break;
+	        //SEARCH CONTACT
 	        case 3:
 	            searchContact();
 	            cin.get(pause);
 	    		system("cls");
 	            break;
+	        //DELETE CONTACT
 	        case 4:
 	            deleteContact();
 	            cin.get(pause);
 	    		system("cls");
 	    		len = 0;
 	            break;
+	        //EDIT CONTACT
 	        case 5:
+	        	editContact();
+	            cin.get(pause);
+	    		system("cls");
+	    		len = 0;
+	    		break;
+	    	//EXIT
+	        case 6:
 	            exit(1);
 	            break;
 	        default:
